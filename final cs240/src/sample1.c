b10b108409bb425b110da7b3d366c118456bab2d
tngnghi <nghitrannguyengia@gmail.com>
2025-05-07 15:25:50 -0700

    Add files via upload

---
+++ final cs240/src/sample1.c
@@ -0,0 +1,8 @@
+int i;
+
+    printf("First 20 numbers:\n");
+    for (i = 1; i <= 20; i++) {
+        printf("%d ", i);
+    }
+
+    printf("\n");
---
+++ final cs240/src/sample2.c
@@ -0,0 +1,7 @@
+int i, sum = 0;
+
+    for (i = 1; i <= 20; i++) {
+        sum += i;
+    }
+
+    printf("Sum of first 20 numbers: %d\n", sum);
