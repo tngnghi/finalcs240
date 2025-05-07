cd33bf82a01b6d64af6f6695db6037e04526c759
tngnghi <nghitrannguyengia@gmail.com>
2025-05-07 15:24:59 -0700

    Update sample.c

--- final cs240/src/sample.c
+++ final cs240/src/sample.c
@@ -1,24 +1,7 @@
-int main() {
-    int a = 5;
-    int b = 10;
-    int c = a + b;
-    int d = c - a;
-    int e = b * 2;
-    int f = e / a;
-    int g = a && b;
-    int h = a || b;
-
-    RED(10);
-    GREEN(20);
-    BLUE(30);
-    BLACK();
-    WHITE();
-    YELLOW();
-    ORANGE(42);
-
-    int rand = 4;
-
-    PINK();
-    int reversed = reverse_bits(b);
-    int emoji = 0x1F600;
+int i;
+for (i = 1; i <= 15; i++) {
+    if (i % 3 == 0 && i % 5 == 0) print("FizzBuzz");
+    else if (i % 3 == 0) print("Fizz");
+    else if (i % 5 == 0) print("Buzz");
+    else print(i);
 }
